8527bdd03724ca6d956a0a35d9b26467e7d87eab
Jamie Gorman <JaVonox@users.noreply.github.com>
2022-05-14 19:50:46 +0100

    Added to script and powerpoint with other methods of similarity

--- SpacySolution/main.py
+++ SpacySolution/main.py
@@ -2,9 +2,13 @@ import spacy
 import pandas as pd
 from spacy.tokens import Doc
 from sklearn.model_selection import train_test_split
+from scipy.spatial import distance
+from scipy import stats
 from spacytextblob.spacytextblob import SpacyTextBlob
 import warnings
 warnings.filterwarnings("ignore") #A lot of the libraries here print out warnings for things like future depreciation. Since this isnt an issue, we can just disable them.
+
+mode = "COSINE" #Modes to use different similarity equations - COSINE/EUCLID/PEARSON
 
 class AIType: #Stores the model training data + the doc file
     def __init__(self):
@@ -20,8 +24,20 @@ class AIType: #Stores the model training data + the doc file
     def CreateDoc(self,docs): #Take all docs and convert into a single doc
         self.model = Doc.from_docs(docs)
 
-    def ReturnSim(self,inputDoc): #Return the similarity score against an input from a trained model
+    def ReturnSimCosine(self,inputDoc): #Return the similarity score against an input from a trained model using cosine similarity
         return self.model.similarity(inputDoc)
+
+    def ReturnSimEuclidean(self,inputDoc): #Return euclidean distance similarity score
+        eucDist = distance.euclidean(self.model.vector,inputDoc.vector) #Scipy incorporates a euclidean distance function
+        eudSim = 1 / (1 + eucDist) #Convert to similarity by using inverse
+        return eudSim
+
+    def ReturnSimPearson(self,inputDoc):
+        perCoef,perP = stats.pearsonr(self.model.vector,inputDoc.vector) #Pearson coefficient similarity
+        #The perCoef is in a range -1 to 1 - making it different from the 0-1 similarity scores from before
+        #However, since it is a ranged value like this, it still works fine as a "score" metric
+        #Though, due to not being in the same range as others, pearson disables some score related features
+        return perCoef
 
 def FormSentence(inVal,topic):
     sentence = ""
@@ -58,10 +74,19 @@ def RemoveBadTokens(text): #Remove the stop words and punctuation from the text
 def ReturnTopicPrediction(docInput): #Returns the top two similarity topics
     gTopic = "UNKNOWN" #The predicted topic
     gSecTopic = "UNKNOWN" #The second predicted topic
-    gTopicScore = 0.0 #The score of the predicted topic
-    gSecTopicScore = 0.0 #The score of the second prediction topic
+    gTopicScore = -1.1 #The score of the predicted topic
+    gSecTopicScore = -1.1 #The score of the second prediction topic
     for top in topicData: #Iterate through each topic model and store the first and second highest scoring topics
-        comparisonVal = topicData[top].ReturnSim(docInput)
+        comparisonVal = 0
+
+        if mode == "COSINE": #Cosine sim
+            comparisonVal = topicData[top].ReturnSimCosine(docInput)
+        elif mode == "EUCLID": #Euclidean distance sim
+            comparisonVal = topicData[top].ReturnSimEuclidean(docInput)
+        elif mode == "PEARSON":
+            comparisonVal = topicData[top].ReturnSimPearson(docInput)
+        else: #Default to cosine
+            comparisonVal = topicData[top].ReturnSimCosine(docInput)
 
         if comparisonVal > gTopicScore: #If greater than the current stored topic score, assign this as the new primary topic
             gSecTopic = gTopic
@@ -80,6 +105,7 @@ inputDataFrame = pd.read_csv(r'.\Dataset\StackExchangeMoney.csv') #Load question
 topicOutput = pd.read_csv(r'.\Dataset\Responses.csv') #Load outputs
 nlp = spacy.load("en_core_web_lg")
 nlp.add_pipe('spacytextblob') #adds sentiment analysis + objectivity to the pipeline
+
 
 x_train_q, x_test_q, y_train_q, y_test_q = train_test_split(inputDataFrame["TYPE"],inputDataFrame["TEXT"], test_size=0.3) #create questions training data
 
@@ -118,6 +144,7 @@ for iter,row in test_q.iterrows(): #Iterate through each test item, getting the 
 
     expTopic,expTopicScore,sTopic,sTopicScore = ReturnTopicPrediction(contents)
 
+
     if expTopic == realType: #Evaluate if the prediction was correct
         score = score + 1
     else:
@@ -126,6 +153,7 @@ for iter,row in test_q.iterrows(): #Iterate through each test item, getting the 
         secCounter = secCounter + 1
     counter = counter + 1
 
+print("Similarity mode: " + mode) #Print the mode of similarity calculation
 print("First Guess Accuracy: " + str(round((score / counter)*100,2)) + "%") #Print the first guess accuracy score
 print("Second Guess Accuracy: " + str(round((secScore / secCounter)*100,2)) + "%") #Print the second guess accuracy score
 
@@ -136,7 +164,7 @@ while True:
 
     expTopic,expTopicScore,sTopic,sTopicScore = ReturnTopicPrediction(inVal)
 
-    if expTopicScore > 0.5:
+    if expTopicScore > 0.5 or mode == "PEARSON": #Pearson model does not use topic score because it is not normalized to 0-1
         #print("Expected topic: " + gTopic + " (similarity: " + str(round(gTopicScore,2)*100) + "%)")
         pass
     else:
@@ -149,7 +177,7 @@ while True:
 
     if expTopic != "UNKNOWN":
 
-        if sTopic != "UNKNOWN" and expTopicScore < 0.9: #If the model is not entirely sure, then give the option to provide a reevaluation of the question
+        if sTopic != "UNKNOWN" and (expTopicScore < 0.9 and mode != "PEARSON"): #If the model is not entirely sure, then give the option to provide a reevaluation of the question
             print("I hope I was able to answer your question! I'm still learning, so please let me know if I got the topic of your question wrong so I can try again!")
             print("(Enter N if you'd like your question reinterpreted)")
             req = input("")
